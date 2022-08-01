99515fb344ebc62c37be31cedf522b60bbee54a8
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-07-31 17:23:38 -0700

    Update exports

--- src/RecursiveTiles.jl
+++ src/RecursiveTiles.jl
@@ -1,5 +1,13 @@
 module RecursiveTiles
 
-# Write your package code here.
+using BenchmarkTools, Test
+
+export findrange, findranges
+export AbstractTile, Tile, tile, tiles
+export AbstractScheme, AbstractExtendScheme, Scheme, ExtendScheme, @scheme
+
+include("schemes.jl")
+include("findrange.jl")
+include("tile.jl")
 
 end
