1f10f0b5456d89e5111a0e1de1eeaff3adbabe3d
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-07-31 19:22:10 -0700

    Remove dead code

--- src/schemes.jl
+++ src/schemes.jl
@@ -109,9 +109,3 @@ end
 macro scheme(f)
     ex = :(Scheme($f, nothing))
 end
-
-# s = @scheme sum last third second first
-# s2 = ExtendScheme(ExtendScheme(ExtendScheme(Scheme(sum, last), third), second), first)
-
-# s3 = @scheme sum last x -> x[begin+2] + 1000 second first
-# s4 = ExtendScheme(ExtendScheme(ExtendScheme(Scheme(sum, last), x -> x[begin+2] + 1000), second), first)
