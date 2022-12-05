6e17619ca2d2b4d571a7549caf88d8a59978b599
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-12-05 08:45:42 -0800

    Tests thereof

--- test/findrange.jl
+++ test/findrange.jl
@@ -23,3 +23,34 @@ end
     @test findranges(abs2, [missing, 0, 0, 1]) == [1:1, 2:3, 4:4]
     @test findranges(identity, [missing, missing, NaN, NaN, 0, 0, 1]) == [1:2, 3:4, 5:6, 7:7]
 end
+
+@testset "findfirstrange" begin
+    @test findfirstrange(isone, [2,3,4]) === nothing
+    @test findfirstrange(isone, [1, 2,3,4]) == 1:1
+    @test findfirstrange(isone, [2,1,3,4]) == 2:2
+    @test findfirstrange(isone, [2,1,1,3,4]) == 2:3
+    @test findfirstrange(isone, [2,1,1,3,1,4]) == 2:3
+    @test findfirstrange(isone, [1,2,1,1,3,1,4]) == 1:1
+    @test findfirstrange(isone, [1,1,2,1,1,3,1,4]) == 1:2
+    @test findfirstrange(isone, [2,3,4,1]) == 4:4
+
+    # Missing, etc.
+    @test_throws TypeError findfirstrange(isone, [missing])
+    @test_throws TypeError findfirstrange(isone, [1, missing])
+
+end
+@testset "findlastrange" begin
+    @test findlastrange(isone, [2,3,4]) === nothing
+    @test findlastrange(isone, [1, 2,3,4]) == 1:1
+    @test findlastrange(isone, [2,1,3,4]) == 2:2
+    @test findlastrange(isone, [2,1,1,3,4]) == 2:3
+    @test findlastrange(isone, [2,1,1,3,1,4]) == 5:5
+    @test findlastrange(isone, [1,2,1,1,3,1,4]) == 6:6
+    @test findlastrange(isone, [1,1,2,1,1,3,1,4]) == 7:7
+    @test findlastrange(isone, [2,3,4,1]) == 4:4
+
+    # Missing, etc.
+    @test_throws TypeError findlastrange(isone, [missing])
+    @test_throws TypeError findfirstrange(isone, [missing, 1])
+
+end
